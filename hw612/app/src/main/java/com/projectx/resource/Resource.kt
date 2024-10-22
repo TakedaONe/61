87c890e67de6eac172bf91e310879348ac147898
TakedaONe <146793652+TakedaONe@users.noreply.github.com>
2024-10-22 08:29:13 -0400

    Delete hw612 directory

--- hw612/app/src/main/java/com/projectx/resource/Resource.kt
+++
@@ -1,9 +0,0 @@
-package com.projectx.resource
-
-sealed class Resource<T> {
-class Loading<T> :Resource<T> ()
-    class Error<T> (val message:String):Resource<T> ()
-    class Success<T>(val data:T):Resource<T> ()
-
-
-}
